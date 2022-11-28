b4dfa078692dd4fbb6ed0f8da3205731d1c24ac4
Lautaro Vazquez <lauti1498@gmail.com>
2022-11-28 15:43:38 -0300

    ejs y css

--- app.js
+++ app.js
@@ -7,3 +7,50 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+let products = []
+
+app.get('/', (req, res) => {
+    try {
+        res.render('form', {
+            titulo: "Ingrese un producto",
+            firstLabel: "Nombre del producto:",
+            secondLabel: "Precio del producto:",
+            thirdLabel: "URL de la imágen del producto:"
+        })
+    } catch (error) {
+        res.send({status: 'Error', message: error.message})
+    }
+})
+
+app.get('/products', (req, res) => {
+    try {
+        res.render('products', {
+            products: products
+        })
+    } catch (error) {
+        res.send({status: 'Error', message: error.message})
+    }
+})
+
+app.post('/', (req, res) => {
+    try {
+        let product = req.body
+        if (!product.product || !product.price || !product.url) {
+            res.send({status: 'Error', message: 'Faltan campos por completar'})
+        } else {
+            console.log('REQ.BODY', req.body)
+            let newId = products.length + 1
+            product.id = newId
+            products.push(product)
+            console.log({message: 'Saved product', product})
+            res.render('products', {
+                products: products
+            }) 
+        } 
+    } catch (error) {
+        res.send({status: 'Error', message: error.message})
+    }
+    
+})
